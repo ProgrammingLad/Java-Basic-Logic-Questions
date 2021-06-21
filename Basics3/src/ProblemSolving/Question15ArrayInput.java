1840cf536d0d5b19fac3d8831c227ecaf513820a
Intel-10th <Intel-10th@Eugene69>
2021-06-21 09:28:54 +0800

    Corrected code

--- Basics3/src/ProblemSolving/Question15ArrayInput.java
+++ Basics3/src/ProblemSolving/Question15ArrayInput.java
@@ -4,32 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/*
- * 	Declare an array and take input from user
- * 	Ask user to enter an integer
- * 	Print index number of that integer
- * 
- * 	If number has a duplicate
- * 	Print all the indexes of that number
- * 
- */
 public class Question15ArrayInput {
 
+	/*
+	 * Declare an array and take input from user 
+	 * Ask user to enter an integer 
+	 * Print the index of that integer
+	 * 
+	 * If number has a duplicate 
+	 * Print all the indexes of that number
+	 * 
+	 */
+
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
-		ArrayList<Integer> numbers = new ArrayList<>(List.of(8, 9, 1, 3, 4, 8, 6, 4));
-		
-		
-		System.out.println("What index do you want to get outt of the array?");
-		
-		Integer indexGetter = numbers.get(scanner.nextInt() - 1);
-		
-		System.out.println("Value: " + indexGetter);
+
+		System.out.println("Please enter a number: ");
+		int userInput = scanner.nextInt();
+
+		ArrayList<Integer> numbers = new ArrayList<>(List.of(8, 9, 1, 3, 4, 8, 6, 4, 16, 8));
+
+		System.out.println("Output: ");
+
+		for(int i = 0; i < numbers.size(); i++) {
+		    if(numbers.get(i) == userInput)
+		        System.out.println(i);
+		 }
 		
 		scanner.close();
-		
-		
+
+		System.out.println();
+		System.out.printf("Desired output: \nFirst index of 8: %d\nSecond index of 8: %d\nThird index of 8: %d ", 0, 5, 9);
 	}
 }
