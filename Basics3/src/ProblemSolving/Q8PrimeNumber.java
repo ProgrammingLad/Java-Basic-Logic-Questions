45e472a060b8ae5ff23c8e5f573a6b4ed850adc0
Intel-10th <Intel-10th@Eugene69>
2021-06-28 12:16:15 +0800

    Minor logic adjustments

--- Basics3/src/ProblemSolving/Q8PrimeNumber.java
+++ Basics3/src/ProblemSolving/Q8PrimeNumber.java
@@ -12,26 +12,26 @@ public class Q8PrimeNumber {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Please input a number: ");
 		int userInput = scanner.nextInt();
-		
+
 		boolean primeNumber = true;
-		
-		for(int i = 2; i < userInput/2; i++) {
-			
-			if(userInput % i == 0) {
+
+		for (int i = 2; i < userInput / 2; i++) {
+
+			if (userInput % i == 0) {
 				primeNumber = false;
 				break;
 			}
 		}
-		
-		if(!primeNumber) {
-			System.out.println(userInput + " is an composite number");
+
+		if (!primeNumber) {
+			System.out.println("No");
 		} else {
-			System.out.println(userInput + " is a prime number");
+			System.out.println("Yes");
 		}
-		
+
 		scanner.close();
 	}
 }
